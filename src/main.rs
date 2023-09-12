a0244bb8ee6832a018fbefd9c9e873674bf30d1e
Gabriel <gabriel.rau@stewart.com>
2023-09-12 18:43:10 -0500

    savin

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,8 @@ use std::error::Error;
 use std::io::BufReader;
 use std::fs::File;
 use std::vec::Vec;
+use geo::*;
 use std::collections::HashMap;
-use rand::prelude::*;
-
-struct Vertex {
-    x: f64,
-    y: f64,
-    is_inserted: bool,
-}
-
-struct Polygon {
-    properties: HashMap<String, String>,
-    edges: Vec<Edge>,
-}
-
-struct Edge {
-    v_0: Vertex,
-    v_1: Vertex, 
-}
-
-trait FisherYates {
-    fn shuffle(&self, rng: &ThreadRng); 
-}
-
-impl FisherYates for Polygon {
-    fn shuffle(&self, rng: &ThreadRng) {
-        for i in (1..self.edges.len()).rev() {
-            let j: usize = rng.gen_range(0..=i);  
-            self.edges.swap(i, j);
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut filein = BufReader::new(File::open("mo_senate_districts.fgb")?);
@@ -47,68 +18,36 @@ fn main() -> Result<(), Box<dyn Error>> {
             panic!("bytes aren't correct")
         }
         let properties = feature.properties()?;
-        let mut edges: Vec<Edge> = Vec::new();
         let mut ind: usize = 0;
+        let mut coords: Vec<Coord> = Vec::new();
         if xy.len() <= 6 {
             panic!("not enough vertices")
         }
         while ind < xy.len() {
-            if ind == 0 {
-                ind += 2; // each entry is either x or y starting with x and alternating
-            }
-            else if ind == xy.len() - 2  {
-                if xy.get(ind) == xy.get(0) && xy.get(ind+1) == xy.get(1) {
-                    // point back to first vertex depends on how file is formated
-                    edges.push(Edge {
-                        v_0: Vertex {
-                            x: xy.get(ind),
-                            y: xy.get(ind+1),
-                            is_inserted:false,
-                        },
-                        v_1: Vertex {
-                            x: xy.get(0),
-                            y: xy.get(1),
-                            is_inserted:false,
-                        },
-                    });
-                }
-            }
-            else {
-                edges.push(Edge {
-                    v_0: Vertex {
-                        x: xy.get(ind),
-                        y: xy.get(ind+1),
-                        is_inserted:false,
-                    },
-                    v_1: Vertex {
-                        x: xy.get(ind),
-                        y: xy.get(ind+1),
-                        is_inserted:false,
-                    },
-                });
-            }
+            coords.push(coord! {
+                x: xy.get(ind),
+                y: xy.get(ind+1),
+            });
+            ind += 2;
         }
-        map_polygons.push(Polygon {
-            properties,
-            edges,
-        });
+        
+        let line_string: LineString<f64> = coords.into_iter().collect();
+        
+        map_polygons.push(Polygon::new(
+                line_string,
+                vec![],
+        ));
+    }
+
+    for mp in map_polygons.iter() {
+        let triangles: Vec<Triangle> = mp.earcut_triangles();
+        let tri = triangles.get(0).unwrap();
+        println!("c1 [ x: {}, y: {} ]\nc2: [ x: {}, y: {} ]\nc3: [ x: {}, y: {} ]\n", tri.0.x, tri.0.y, tri.1.x, tri.1.y, tri.2.x, tri.2.y); 
     }
 
     Ok(())
 }
 
 
-
-fn triangulate(map_polygons: &Vec<Polygon>) -> Vec<f64> {
-    let mut rng = thread_rng();
-    let triangles_vec: Vec<f64> = Vec::from([]);
-    if map_polygons.len() == 0 {
-        triangles_vec
-    }
-
-
-
-
-}
 
 
